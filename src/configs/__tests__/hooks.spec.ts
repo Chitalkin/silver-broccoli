d4b76f8c3a330b54a1b27104e7eaf17c42cdb901
Nikita Puzidarov <NPuzidarov@it-one.ru>
2022-09-21 13:41:57 +0300

    test(useConfig): added new tests

--- src/configs/__tests__/hooks.spec.ts
+++ src/configs/__tests__/hooks.spec.ts
@@ -1,18 +1,24 @@
-import { renderHook } from '@testing-library/react';
-import { ESettingsConfigItem, SettingsConfig } from '@/configs';
-import { useDefaultConfigValue } from '../hooks/useDefaultConfigValue';
+import { renderHook, act } from '@testing-library/react';
+import {
+  ERandomPercentage,
+  ESettingsConfigItem,
+  SettingsConfig,
+} from '@/configs';
+import * as defaultSettings from '../hooks/useDefaultConfigValue';
 import { useConfig } from '../hooks/useConfig';
 
 describe('Settings hooks', () => {
   describe('useDefaultConfig', () => {
     it('should return small for BoardSizeConfig', () => {
-      const result = useDefaultConfigValue(ESettingsConfigItem.BoardSizeConfig);
+      const result = defaultSettings.useDefaultConfigValue(
+        ESettingsConfigItem.BoardSizeConfig,
+      );
 
       expect(result).toEqual(SettingsConfig.boardSizeConfig.small);
     });
 
     it('should return none for RandomFillPersentageConfig', () => {
-      const result = useDefaultConfigValue(
+      const result = defaultSettings.useDefaultConfigValue(
         ESettingsConfigItem.RandomFillPersentageConfig,
       );
 
@@ -20,31 +26,104 @@ describe('Settings hooks', () => {
     });
 
     it('should return slow for SimSpeedConfig', () => {
-      const result = useDefaultConfigValue(ESettingsConfigItem.SimSpeedConfig);
+      const result = defaultSettings.useDefaultConfigValue(
+        ESettingsConfigItem.SimSpeedConfig,
+      );
 
       expect(result).toEqual(SettingsConfig.simSpeedConfig.slow);
     });
 
     it('should throw error if config is invalid', () => {
-      expect(useDefaultConfigValue).toThrowError(new Error('Invalid config'));
+      expect(defaultSettings.useDefaultConfigValue).toThrowError(
+        new Error('Invalid config'),
+      );
     });
   });
 
   describe('useConfig', () => {
-    // describe('should return the expected config by config item', () => {
-    //   const { result } = renderHook(() => jest.fn);
-    //   it.each([
-    //     [ESettingsConfigItem.BoardSizeConfig, SettingsConfig.boardSizeConfig],
-    //     [
-    //       ESettingsConfigItem.RandomFillPersentageConfig,
-    //       SettingsConfig.randomFillPersentageConfig,
-    //     ],
-    //     [ESettingsConfigItem.BoardSizeConfig, SettingsConfig.simSpeedConfig],
-    //   ])(`%s as config item, should return %o`, (item, expected) => {
-    //     expect(useConfig(item)).toEqual([undefined, expected, undefined]);
-    //   });
-    // });
-    // it('should call useDefaultConfig to get the default value', () => {
-    // });
+    it('should call useDefaultConfigValue', () => {
+      const defaultConfigSpy = jest.spyOn(
+        defaultSettings,
+        'useDefaultConfigValue',
+      );
+
+      renderHook(() => useConfig(ESettingsConfigItem.BoardSizeConfig));
+
+      expect(defaultConfigSpy).toHaveBeenCalledTimes(1);
+
+      defaultConfigSpy.mockRestore();
+    });
+
+    it('should change config value', () => {
+      const { result } = renderHook(() =>
+        useConfig(ESettingsConfigItem.RandomFillPersentageConfig),
+      );
+
+      act(() => {
+        // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+        // @ts-ignore
+        result.current[2](ERandomPercentage.High);
+      });
+
+      expect(result.current[0]).toBe(
+        SettingsConfig.randomFillPersentageConfig[ERandomPercentage.High],
+      );
+    });
+
+    it('should return the expected config settings for BoardSizeConfig', () => {
+      const { result } = renderHook(() =>
+        useConfig(ESettingsConfigItem.BoardSizeConfig),
+      );
+
+      expect(result.current).toEqual(
+        expect.arrayContaining([
+          expect.any(Object),
+          SettingsConfig.boardSizeConfig,
+          expect.any(Function),
+        ]),
+      );
+    });
+
+    it('should return the expected config settings for boardSizeConfig', () => {
+      const { result } = renderHook(() =>
+        useConfig(ESettingsConfigItem.BoardSizeConfig),
+      );
+
+      expect(result.current).toEqual(
+        expect.arrayContaining([
+          expect.any(Object),
+          SettingsConfig.boardSizeConfig,
+          expect.any(Function),
+        ]),
+      );
+    });
+
+    it('should return the expected config settings for simSpeedConfig', () => {
+      const { result } = renderHook(() =>
+        useConfig(ESettingsConfigItem.SimSpeedConfig),
+      );
+
+      expect(result.current).toEqual(
+        expect.arrayContaining([
+          expect.any(Number),
+          SettingsConfig.simSpeedConfig,
+          expect.any(Function),
+        ]),
+      );
+    });
+
+    it('should return the expected config settings for randomFillPersentageConfig', () => {
+      const { result } = renderHook(() =>
+        useConfig(ESettingsConfigItem.RandomFillPersentageConfig),
+      );
+
+      expect(result.current).toEqual(
+        expect.arrayContaining([
+          expect.any(Number),
+          SettingsConfig.randomFillPersentageConfig,
+          expect.any(Function),
+        ]),
+      );
+    });
   });
 });
